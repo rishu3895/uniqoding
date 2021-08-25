abc5e88ec53ca6afa494688811056e4518dbadc1
rishu3895 <rishabh.rsharma1999@gmail.com>
2021-08-25 22:38:27 +0530

    Started file

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { Navigation } from "./components/navigation";
+import { Header } from "./components/header";
+import { Features } from "./components/features";
+import { About } from "./components/about";
+import { Services } from "./components/services";
+import { Gallery } from "./components/gallery";
+import { Testimonials } from "./components/testimonials";
+import { Team } from "./components/Team";
+import { Contact } from "./components/contact";
+import JsonData from "./data/data.json";
+const App = () => {
+    const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+  return (
+    <div>
+      <Navigation data={landingPageData.Title}/>
+      <Header data={landingPageData.Header} />
+      <Features data={landingPageData.Features} />
+      <About data={landingPageData.About} />
+      <Services data={landingPageData.Services} />
+      {/* <Gallery /> */}
+      <Testimonials data={landingPageData.Testimonials} />
+      <Team data={landingPageData.Team} />
+      <Contact data={landingPageData.Contact} />
+    </div>
+  );
+};
+
+export default App;
